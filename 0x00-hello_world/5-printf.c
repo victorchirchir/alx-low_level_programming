c013bd46acabbff03408a562940316c78f9149d4
victorchirchir <vctrchirchir@gmail.com>
2023-03-16 22:49:56 +0000

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main-prints out a string to stdout
+ * Return:on success returns 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
