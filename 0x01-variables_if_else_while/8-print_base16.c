2b5efb24ba2b83f17910b1f282dfc78e39f04c07
victorchirchir <vctrchirchir@gmail.com>
2023-03-17 23:32:24 +0000

    added 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *Description:main-prints out numbers of base sixteen
+ *Return:Always successful
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 16 ; i++)
+	{
+		if (i < 10)
+		{
+		putchar(i + '0');
+		}
+		else
+		{
+		putchar(i - 10 + 'a');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
