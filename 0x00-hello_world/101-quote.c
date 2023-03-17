da6a4391c3375c7e7be545113ace72fab966763d
victorchirchir <vctrchirchir@gmail.com>
2023-03-17 00:02:38 +0000

    fixed 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <unistd.h>
 /**
  * Description: main- prints out a string standard error stream
- * Return: returns 0 if successful
+ * Return: returns 1 if successful
 */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	return (1);
 }
