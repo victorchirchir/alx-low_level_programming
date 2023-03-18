f97bd27458e639c658b3a2a0da2f5250c047afc7
victorchirchir <vctrchirchir@gmail.com>
2023-03-18 00:57:52 +0000

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ *Description:main-lists pair of combinations of digits
+ *Return:always successful
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100 ; i++)
+	{
+		for (j = i; j < 100 ; j++)
+		{
+			if (i != j)
+			{
+				putchar(i / 10 + '0');
+				putchar(i % 10 + '0');
+				putchar(' ');
+				putchar(j / 10 + '0');
+				putchar(j % 10 + '0');
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
