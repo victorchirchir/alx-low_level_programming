b84bc3eb958e70914917e9d2b9c4ef5396ac63dc
victorchirchir <vctrchirchir@gmail.com>
2023-03-21 22:54:13 +0000

    added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add:-add two numbers
+ * @x :-number to be added
+ * @y :bumber to be added
+ * Return:result from addition
+ */
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * times_table-print out the times table
+ * Return:void
+ */
+void times_table(void)
+{
+	int a, b, op;
+
+		for (a = 0; a <= 9; a++)
+		{
