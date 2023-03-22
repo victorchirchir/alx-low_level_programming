b97c3e80be5b3bf863d31974860bf4eabaf42325
victorchirchir <vctrchirchir@gmail.com>
2023-03-22 18:01:29 +0000

    added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,27 @@
  */
 void times_table(void)
 {
-	int a, b, op;
+	int a, b, result;
 
 		for (a = 0; a <= 9; a++)
 		{
+			_putchar(48);
+			for (b = 0; b <= 9; b++)
+			{
+				_putchar(44);
+				_putchar(32);
+				result = a * b;
+				if (result < 10)
+				{
+					_putchar(32);
+					_putchar(result + 48);
+				}
+				else
+				{
+					_putchar((result / 10) + 48);
+					_putchar((result % 10) + 48);
+				}
+			}
+			_putchar('\n');
+		}
+}
