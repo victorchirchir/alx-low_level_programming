ff54d6d826708de721d3ed8fd3498ae5bc635ae9
victorchirchir <vctrchirchir@gmail.com>
2023-03-24 01:48:13 +0000

    added 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main-entry point
+ * Return:0 on success
+ */
+int main(void)
+{
+	long int n, f, lf;
+
+	n = 612852475143;
+	f = 2;
+	lf = 0;
+
+	while (n > 1)
+	{
+		if (n % f == 0)
+		{
+			lf = f;
+			n /= f;
+		}
+		else
+		{
+			f++
+		}
+	}
+	printf("%ld\n", lf);
+	return (0);
+}
