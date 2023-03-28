a6372bb9905c141fafb153da6c5b14eb72285c86
victorchirchir <vctrchirchir@gmail.com>
2023-03-28 22:23:18 +0000

    added 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main-prints out a random password
+ * Return-0 on success
+ */
+#define PASSWORD_LENGTH 10
+
+int main(void)
+{
+	char password[PASSWORD_LENGTH + 1];
+
+	srand(time(NULL));
+
+	char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	int i;
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		int index;
+
+		index = rand() % sizeof(charset);
+		password[i] = charset[index];
+	}
+	password[PASSWORD_LENGTH] = '\0';
+	printf("%s\n", password);
+	return (0);
+}
