de4eb95ae315eb3f71e3b6fc818ed7182ef6fc7a
victorchirchir <vctrchirchir@gmail.com>
2023-03-29 23:17:19 +0000

    added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *string_toupper-converts lower string to upper
+ *@s:string pointer
+ *Return: char
+*/
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - ('a' - 'A');
+		}
+		i++;
+	}
+	return (s);
+}
