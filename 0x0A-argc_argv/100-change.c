9dccf33eb0e942f82e61aede60f46e7f15cd0bf9
victorchirchir <vctrchirchir@gmail.com>
2023-04-07 23:14:25 +0000

    added 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,21 +20,28 @@ int main(int argc, char *argv[])
 		cents = atoi(argv[1]);
 		num_coins = 0;
 
-		num_coins += cents / 25;
-		cents %= 25;
-
-		num_coins += cents / 10;
-		cents %= 10;
-
-		num_coins += cents / 5;
-		cents %= 5;
-
-		num_coins += cents / 2;
-		cents %= 2;
-
-		num_coins += cents;
-
-		printf("%d\n", num_coins);
+		if (cents < 0)
+		{
+			printf("%d\n", 0);
+		}
+		else
+		{
+			num_coins += cents / 25;
+			cents %= 25;
+			
+			num_coins += cents / 10;
+			cents %= 10;
+			
+			num_coins += cents / 5;
+			cents %= 5;
+			
+			num_coins += cents / 2;
+			cents %= 2;
+			
+			num_coins += cents;
+			
+			printf("%d\n", num_coins);
+		}
 	}
 	return (0);
 }
