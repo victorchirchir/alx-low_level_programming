fb35a8fc2ce1c840e32f4a73317ece503577b9b7
victorchirchir <vctrchirchir@gmail.com>
2023-04-07 23:06:17 +0000

    added 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main-gives number of conis returned
+ * @argc:no of parameters passed to the program
+ * @argv:array of string passed to the program
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int num_coins, cents;
+
+		cents = atoi(argv[1]);
+		num_coins = 0;
+
+		num_coins += cents / 25;
+		cents %= 25;
+
+		num_coins += cents / 10;
+		cents %= 10;
+
+		num_coins += cents / 5;
+		cents %= 5;
+
+		num_coins += cents / 2;
+		cents %= 2;
+
+		num_coins += cents;
+
+		printf("%d\n", num_coins);
+	}
+	return (0);
+}
