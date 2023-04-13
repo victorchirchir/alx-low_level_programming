74cf972b927e913142211f93db26ee977620e27a
victorchirchir <vctrchirchir@gmail.com>
2023-04-13 21:42:13 +0000

    added 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int size, i;
 	char *src, *dest;
 
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
@@ -34,6 +34,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			dest[i] = src[i];
 		}
 	}
-	return (ptr);
+	return (new_ptr);
 	free(ptr);
 }
