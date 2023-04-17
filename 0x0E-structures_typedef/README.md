5dc382391cf565afa70614bac4c005d22f6ec05d
victorchirchir <vctrchirchir@gmail.com>
2023-04-17 21:32:38 +0000

    added dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,3 @@
+# Readme for dir 0x0E-structures_typedef
+1. This directory is for tasks done on structures and typdef tasks
+#C_is_fun
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - struct to store details of dogs
+ * @name:name of the dog
+ * @age:age of the dog
+ * @owner:who owns the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
