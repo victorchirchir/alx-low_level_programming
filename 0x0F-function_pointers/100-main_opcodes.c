e118f767d718ac0b1d4f8c41d58ee30f76cb335d
victorchirchir <vctrchirchir@gmail.com>
2023-04-20 16:08:53 +0000

    added 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,24 +8,25 @@
  */
 int main(int argc, char *argv[])
 {
-	int num = atoi(argv[1]);
 	char *ptr = (char *)main;
-	int i;
+	int i, num;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
+	num = atoi(argv[1]);
 	if (num <= 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-	for (i = 0; i < num; i++)
+	for (i = 0; i < (num - 1); i++)
 	{
 		printf("%02x ", ptr[i] & 0xFF);
 	}
+	printf("%02x", ptr[num - 1] & 0xFF);
 	printf("\n");
 	return (0);
 }
