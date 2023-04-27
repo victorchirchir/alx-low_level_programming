b904b19c64202050d1eacd9c454e988bf799a1f4
victorchirchir <vctrchirchir@gmail.com>
2023-04-27 20:30:13 +0000

    added 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_list - prints elements of list_t and returns no of nodes
+ * @h:struct item
+ * Return:number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+			h = h->next
+		}
+		counter++;
+	}
+	return (counter);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+# README for directory 0x12-singly_linked_lists
+this was done for tasks for linked_lists tasks
+
