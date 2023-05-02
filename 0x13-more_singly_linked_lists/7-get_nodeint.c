7668d0b20a4c2fd15e5e6f42676ba986e0ba72b2
victorchirchir <vctrchirchir@gmail.com>
2023-05-02 16:03:38 +0000

    added 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -15,6 +15,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head)
 	{
 		current = head;
+		if (index == 0)
+		{
+			return (head);
+		}
 
 		for (i = 0; i < index; i++)
 		{
