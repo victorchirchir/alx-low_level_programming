3df9486c2d2efbc183601cf95c9a7f06bd1bbc9d
victorchirchir <vctrchirchir@gmail.com>
2023-05-02 17:28:42 +0000

    added 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		new_node->n = n;
+		*head = new_node;
+	}
 	current = *head;
 	while (current != NULL)
 	{
