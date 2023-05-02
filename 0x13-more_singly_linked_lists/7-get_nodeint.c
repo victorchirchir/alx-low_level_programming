d73841fad532d55ef25e795cc623222679df03d8
victorchirchir <vctrchirchir@gmail.com>
2023-05-02 15:50:06 +0000

    added 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns node of an index passed
+ * @head:linked list passed
+ * @index:ndex of the node to be retrieved
+ * Return:node of the index passed
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *current;
+	listint_t *curr_node;
+	unsigned int i;
+
+	if (head)
+	{
+		current = head;
+
+		for (i = 0; i < index; i++)
+		{
+			current = current->next;
+		}
+		curr_node = current;
+		if (curr_node == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (curr_node);
+}
+
+
