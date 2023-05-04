ff79ad3c1e776f10f075a903ca57cc3387a00604
victorchirchir <vctrchirchir@gmail.com>
2023-05-04 00:08:28 +0000

    added 101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,18 +10,30 @@ size_t print_listint_safe(const listint_t *head)
 	size_t count = 0;
 	const listint_t *curr = head;
 	const listint_t *prev = NULL;
+	size_t checker;
 
 	while (curr != NULL)
 	{
 		printf("[%p] %d\n", (void *) curr, curr->n);
-		count++;
-		prev = curr;
-		curr = curr->next;
 
-		if (prev <= curr)
+		count++;
+		curr = curr->next;
+		checker = 0;
+		prev = head;
+		if (!head)
 		{
-			printf("-> [%p] %d\n", (void *)curr, curr->n);
 			exit(98);
+		}
+
+		while (checker < count)
+		{
+			if (prev == curr)
+			{
+				printf("-> [%p] %d\n", (void *)curr, curr->n);
+				return (count);
+			}
+			prev = prev->next;
+			checker++;
 		}
 
 	}
