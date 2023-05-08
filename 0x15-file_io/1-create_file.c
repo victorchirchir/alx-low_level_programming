22689968a906508b494326a4953aaaabd74872ea
victorchirchir <vctrchirchir@gmail.com>
2023-05-08 21:20:20 +0000

    added 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * count_string - count chars in a string
+ * @str:string
+ * Return:returns no of chars in a string
+ */
+int count_string(char *str)
+{
+	int len;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+/**
+ * create_file - creates file
+ * @filename:file to create
+ * @text_content:string to write to the file
+ * Return:1 on success -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, w;
+	mode_t mode = S_IRUSR | S_IWUSR;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	if (fd < 0)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		len = count_string(text_content);
+		w = write(fd, text_content, len);
+		if (w < 0)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
+
+
+
+
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,5 +5,9 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int count_string(char *str);
 #endif
