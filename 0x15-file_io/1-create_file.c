552af61504dce6bd596396dc08382b879c01ab8b
victorchirchir <vctrchirchir@gmail.com>
2023-05-08 21:27:11 +0000

    added 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -30,7 +30,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, mode);
 	if (fd < 0)
 	{
 		return (-1);
